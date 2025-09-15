968a0ec776e9fe66b6379bd7c7d29fba5e80065e
Lucas Ribeiro <lucas.ribeiro@ptarental.com.br>
2025-09-15 11:12:25 -0300

    mudança no vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: "/Portfolio-Lucas/", // <<< coloque o NOME do repositório aqui
+  base: "/Portifolio-Lucas/", 
   plugins: [react()],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
